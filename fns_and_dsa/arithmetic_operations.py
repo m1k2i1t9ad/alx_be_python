47f50cec0740c7f27697ea8ac083b6b6bac05304
m1k2i1t9ad <michaelt5593@gmail.com>
2024-09-28 10:52:33 +0300

    Refactor arithmetic_operations.py to use if-elif-else for operations

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -2,17 +2,16 @@
 
 def perform_operation(num1, num2, operation):
     """Perform basic arithmetic operations."""
-    match operation:
-        case 'add':
-            return num1 + num2
-        case 'subtract':
-            return num1 - num2
-        case 'multiply':
-            return num1 * num2
-        case 'divide':
-            if num2 == 0:
-                return "Error: Division by zero."
-            else:
-                return num1 / num2
-        case _:
-            return "Error: Invalid operation."
+    if operation == 'add':
+        return num1 + num2
+    elif operation == 'subtract':
+        return num1 - num2
+    elif operation == 'multiply':
+        return num1 * num2
+    elif operation == 'divide':
+        if num2 == 0:
+            return "Error: Division by zero."
+        else:
+            return num1 / num2
+    else:
+        return "Error: Invalid operation."
