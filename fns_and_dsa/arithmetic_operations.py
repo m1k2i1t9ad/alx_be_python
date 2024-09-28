16a59837588369cd9f462eaa804911785da5dee3
m1k2i1t9ad <michaelt5593@gmail.com>
2024-09-28 10:42:19 +0300

    Add arithmetic_operations.py with perform_operation function

---
+++ fns_and_dsa/arithmetic_operations.py
@@ -0,0 +1,17 @@
+# arithmetic_operations.py
+
+def perform_operation(num1, num2, operation):
+    """Perform basic arithmetic operations."""
+    match operation:
+        case 'add':
+            return num1 + num2
+        case 'subtract':
+            return num1 - num2
+        case 'multiply':
+            return num1 * num2
+        case 'divide':
+            if num2 == 0:
+                return "Error: Division by zero."
+            return num1 / num2
+        case _:
+            return "Error: Invalid operation."
